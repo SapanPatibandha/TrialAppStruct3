a905ae32765da5d9984749f4a4f11a18a311bad1
Sapan Patibandha <sapan.patibandha@contis.com>
2020-05-23 13:17:09 +0530

    Add project files.

---
+++ TrialAppStrruct3.Infrastructure.Persistence/Data/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrialAppStrruct3.Infrastructure.Persistence.Data
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
